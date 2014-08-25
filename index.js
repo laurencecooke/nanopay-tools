b9c1728cb9c466643b7fe4df86d87721f25c9236
Alex <alex@nanopay.net>
2014-08-25 09:57:17 -0400

    Minor refactoring

--- index.js
+++ index.js
@@ -8,28 +8,23 @@
  */
 var $promise = require('bluebird');
 
-module.exports = tools;
+var cls = {};
 
-function tools(app) {
+cls.polymorph = function () {
+  return function () {
+    if (typeof sync == 'undefined') {
+      var sync = func;
+      var async = new $promise.promisify(sync);
+    }
 
-  var bluebird = {};
+    var args = [].slice.call(arguments);
 
-  bluebird.polymorph = function (func) {
-    return function () {
-      if (typeof sync == 'undefined') {
-        var sync = func;
-        var async = new $promise.promisify(sync);
-      }
-
-      var args = [].slice.call(arguments);
-
-      if (typeof args.slice(-1)[0] == 'function') {
-        return sync.apply(this, arguments);
-      } else {
-        return async.apply(this, arguments);
-      }
-    };
+    if (typeof args.slice(-1)[0] == 'function') {
+      return sync.apply(this, arguments);
+    } else {
+      return async.apply(this, arguments);
+    }
   }
-
-  return bluebird;
 }
+
+module.exports = cls;
